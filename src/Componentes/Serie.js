adf2ec345e357bd7a3eb10f34e4ae354a81898e0
Malena de Juan <malena.dj4@gmail.com>
2021-03-23 10:22:47 +0100

    anyadido componente serie

---
+++ src/Componentes/Serie.js
@@ -0,0 +1,7 @@
+const Serie = () => {
+  return (
+    <li className="serie"><img className="poster" src="https://m.media-amazon.com/images/M/MV5BMTQzODk5N2UtYjE4ZC00YWM1LWJkMGYtYmY2NjNjMjIzZDRmXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg" alt="Six feet under" />Six feet under (HBO, año 2001)</li>
+  );
+};
+
+export default Serie;
