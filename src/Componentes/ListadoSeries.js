6d905a656073b08fbfe7309a05ca0605afef4572
Malena de Juan <malena.dj4@gmail.com>
2021-03-23 10:14:13 +0100

    division por componentes

---
+++ src/Componentes/ListadoSeries.js
@@ -0,0 +1,16 @@
+import TotalSeries from "./TotalSeries";
+
+const ListadoSeries = () => {
+  return (
+    <section className="listado">
+      <TotalSeries />
+      <ul className="series">
+        <li className="serie"><img className="poster" src="https://m.media-amazon.com/images/M/MV5BMTQzODk5N2UtYjE4ZC00YWM1LWJkMGYtYmY2NjNjMjIzZDRmXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg" alt="Six feet under" />Six feet under (HBO, año 2001)</li>
+        <li className="serie"><img className="poster" src="img/tv.jpg" alt="Six feet under" />Casi feliz (Netflix, año 2020)</li>
+        <li className="serie"><img className="poster" src="https://m.media-amazon.com/images/M/MV5BYjg1ZmQ0ZDktMjNhZS00MDJjLTllZTItMmIyYTBkYjM2MzQzXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg" alt="Six feet under" />Atypical (Netflix, año 2017)</li>
+      </ul>
+    </section>
+  );
+};
+
+export default ListadoSeries;
